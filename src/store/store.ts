378761218a1500efaa2869088f7cbf6e3be1ea36
MacBook Air <macbookair@Noutbuk-MacBook.local>
2024-11-14 11:46:52 +0300

    fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './reducers/user.slice';
-import nodeReducer from "./reducers/node.slice"
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        node: nodeReducer
     }
 })
 
